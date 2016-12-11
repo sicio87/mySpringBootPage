d656a3f97bac147fbc86d52841b7633579167900
Karol Sićko <karol.sicko@gmail.com>
2016-12-11 14:39:31 +0100

    commit

--- src/main/java/eu/karols/controller/IndexController.java
+++ src/main/java/eu/karols/controller/IndexController.java
@@ -1,20 +1,13 @@
 package eu.karols.controller;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
-	// inject via application.properties
-	@Value("${index.message:test}")
-	private String message = "Hello World";
 
 	@RequestMapping("/")
-	public String index(Map<String, Object> model) {
-		model.put("message", this.message);
+	public String index() {
 		return "index";
 	}
 }
